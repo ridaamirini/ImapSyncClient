4cfd43d663277dfd3fabb7e62dc7769829e61bb9
Rida Amirini <ridaamirini@rs-apps.de>
2020-01-26 15:43:44 +0100

    #20 resolved and refactored imapsync.js

--- src/main/imapsync.js
+++ src/main/imapsync.js
@@ -1,8 +1,8 @@
 import is from 'electron-is';
 import fs from 'fs';
 import path from 'path';
-import { exec } from 'child_process';
-import { remote } from 'electron';
+import {execFile} from 'child_process';
+import {remote} from 'electron';
 
 // Childprocess id
 let childProcess = null;
@@ -10,77 +10,77 @@ const pidFilePath = path.join(remote.app.getPath('temp'), '/imapsync.pid');
 
 // Imapsync command options
 const commandOptions = [
-  '--nolog',
-  '--pidfile ' + pidFilePath,
-  '--pidfilelocking'
+    '--nolog',
+    '--noreleasecheck',
+    '--pidfile=' + pidFilePath,
+    '--pidfilelocking'
 ];
 
-function getExecutablePath () {
-  if (is.windows()) {
-    return path.join(__static, '/windows/imapsync/imapsync.exe');
-  }
-  else if (is.macOS()) {
-    return path.join(__static, '/bin/imapsync_bin_darwin');
-  }
-  else if (is.linux()) {
-    return path.join(__static, '/bin/imapsync');
-  }
+function getExecutablePath() {
+    let binaryPath = null;
+
+    if (is.windows()) {
+        binaryPath = 'windows/imapsync.exe';
+    }
+    else if (is.macOS()) {
+        binaryPath = 'darwin/imapsync';
+    }
+    else if (is.linux()) {
+        binaryPath = 'bin/imapsync';
+    }
+
+    return path.join(__static, 'imapsync_binaries/', binaryPath);
 }
 
-function convertToCommandParams (mailbox) {
-  let params = [
-    '--host1',
-    mailbox.imap_from,
-    '--user1',
-    mailbox.mailbox_from,
-    '--password1',
-    mailbox.password_from,
-    '--host2',
-    mailbox.imap_to,
-    '--user2',
-    mailbox.mailbox_to,
-    '--password2',
-    mailbox.password_to
-  ];
-
-  return params.join(' ');
+function mapImapsyncCommandArgs(mailbox) {
+    return [
+        '--host1=' + mailbox.imap_from,
+        '--user1=' + mailbox.mailbox_from,
+        '--password1=' + mailbox.password_from,
+        '--host2=' + mailbox.imap_to,
+        '--user2=' + mailbox.mailbox_to,
+        '--password2=' + mailbox.password_to
+    ];
 }
 
-function imapsync (mailbox) {
-  return new Promise((resolve, reject) => {
-    let executable = getExecutablePath();
-    let cmd = executable +
-              ' ' +
-              commandOptions.join(' ') +
-              ' ' +
-              convertToCommandParams(mailbox);
+function imapsync(mailbox) {
+    return new Promise((resolve, reject) => {
+        let messages = {
+            success: null,
+            failure: null
+        };
 
-    childProcess = exec(
-        cmd,
-        {encoding: 'utf8'},
-        (error, stdout, stderr) => {
-            const failure = error || stderr;
-            if (childProcess.exitCode !== 0 && failure) {
-                reject(failure);
+        childProcess = execFile(
+            getExecutablePath(),
+            [...mapImapsyncCommandArgs(mailbox), ...commandOptions],
+            {encoding: 'utf8'},
+            (error, stdout, stderr) => {
+                messages.success = stdout;
+                messages.failure = error || stderr;
+            }
+        );
+
+        childProcess.on('close', code => {
+            if (code !== 0 && messages.failure) {
+                reject(messages.failure);
             }
 
-            resolve(stdout);
-        }
-    );
-  });
+            resolve(messages.success);
+        });
+    });
 }
 
-function abortImapsync () {
-  childProcess.kill('SIGINT');
+function abortImapsync() {
+    childProcess.kill('SIGINT');
 
-  if (fs.existsSync(pidFilePath)) {
-    fs.unlink(pidFilePath, error => {
-      if (error) console.error(error);
-    });
-  }
+    if (fs.existsSync(pidFilePath)) {
+        fs.unlink(pidFilePath, error => {
+            if (error) console.error(error);
+        });
+    }
 }
 
 export {
-  abortImapsync,
-  imapsync
+    abortImapsync,
+    imapsync
 };
